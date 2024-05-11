4bf19076ab3d0a908b3b4ebdf1fd6172569d1f68
Dogiakno <105819337+Dogiakno@users.noreply.github.com>
2024-05-11 21:11:17 +0300

    Update robot.ino

--- robot.ino
+++ robot.ino
@@ -1,34 +1,21 @@
 // Pin tanımları
-#include <Wire.h>
-#include "Adafruit_TCS34725.h"
-
-#define trigPin1 9
-#define echoPin1 10
-#define trigPin2 11
-#define echoPin2 12
-#define trigPin3 7
-#define echoPin3 8
-
-#define Rpwm1 6
-#define RDir1 7
-#define RDir2 8
-#define Lpwm1 11
-#define LDir1 10
-#define LDir2 9
-#define ObjectSens 2
-#define LQ1 A7
-#define LQ2 A6
-#define LQ3 A5
-#define LQ4 A4
-#define LQ5 A3
-#define LQ6 A2
-#define LQ7 A1
-#define LQ8 A0
-#define SW1 12
-#define SW2 13
+#include <QTRSensors.h>
+#define NUM_SENSORS 8  
+#define Rpwm1 2
+#define RDir1 3
+#define RDir2 4
+#define Lpwm1 7
+#define LDir1 5
+#define LDir2 6
+#define LQ1 A0
+#define LQ2 A1
+#define LQ3 A2
+#define LQ4 A3
+#define LQ5 A4
+#define LQ6 A5
+#define LQ7 A6
+#define LQ8 A7
 #define Led 13
-
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
 
 // Değişkenler
 
@@ -38,8 +25,6 @@ float RightBaseSpeed = 220; // Sağ motorun varsayılan hızı
 float Kp = 0.07; // P terimi
 float Kd = 2; // D terimi
 float Ki = 0.0001; // I terimi
-
-int yellowBoxCount = 0;
 
 int Q[8] = {LQ8, LQ7, LQ6, LQ5, LQ4, LQ3, LQ2, LQ1}; // Çizgi sensörlerinin pinleri
 int SensMin[8]; // Sensörlerin minimum değerleri
@@ -134,26 +119,7 @@ int ReadSensor() {
 
 // Setup fonksiyonu - Başlangıç ayarları
 void setup() {
- Serial.begin(9600);
-  pinMode(trigPin1, OUTPUT);
-  pinMode(echoPin1, INPUT);
-  pinMode(trigPin2, OUTPUT);
-  pinMode(echoPin2, INPUT);
-  pinMode(trigPin3, OUTPUT);
-  pinMode(echoPin3, INPUT);
-
-  if (tcs.begin()) {
-    Serial.println("Sensor detected.");
-  } else {
-    Serial.println("Sensor not detected ...Please check your connections.");
-    while (1);
-  }
-
-pinMode(trigPin, OUTPUT);
-pinMode(echoPin, INPUT);
-Serial.println("Arduino İle Mesafe Sensörü Uygulaması Başlatılıyor...");
-delay(3000);
-  
+Serial.begin(9600);
   // Pin modları ayarlanıyor
   pinMode(Rpwm1, OUTPUT);
   pinMode(Lpwm1, OUTPUT);
@@ -169,10 +135,8 @@ delay(3000);
   pinMode(LQ6, INPUT);
   pinMode(LQ7, INPUT);
   pinMode(LQ8, INPUT);
-  pinMode(ObjectSens, INPUT);
   pinMode(Led, OUTPUT);
-  pinMode(SW1, INPUT);
-  pinMode(SW2, INPUT);
+
   
   MotorsSpeed(0, 0); // Motorları durdur
   delay(2000); // Başlangıç için 2 saniye bekle
@@ -198,75 +162,7 @@ delay(3000);
 // Ana döngü
 void loop() {
 
-  uint16_t r, g, b, c;
-  long duration1, distance1, duration2, distance2, duration3, distance3;
-
-  tcs.getRawData(&r, &g, &b, &c);
-
-  // Cap the RGB values between 0 and 255
-  /*r = constrain(map(r, 0, 65535, 0, 1000), 0, 1000);
-  g = constrain(map(g, 0, 65535, 0, 1000), 0, 1000);
-  b = constrain(map(b, 0, 65535, 0, 1000), 0, 1000);*/
-
-  Serial.print("Red: "); Serial.print(r); Serial.print(" ");
-  Serial.print("Green: "); Serial.print(g); Serial.print(" ");
-  Serial.print("Blue: "); Serial.println(b);
-
-  if(r > g + b) {
-    Serial.print("RED")
-  } else if ((g < r)&&(b < r)) {
-    Serial.print("BLUE")
-  } else if ((b > r))&&(b > g)) {
-    Serial.print("YELLOW")
-  }
-
-  digitalWrite(trigPin1, LOW);
-  digitalWrite(trigPin2, LOW);
-  digitalWrite(trigPin3, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin1, HIGH);
-  digitalWrite(trigPin2, HIGH);
-  digitalWrite(trigPin3, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin1, LOW);
-  digitalWrite(trigPin2, LOW);
-  digitalWrite(trigPin3, LOW);
-  duration1 = pulseIn(echoPin1, HIGH);
-  duration2 = pulseIn(echoPin2, HIGH);
-  duration3 = pulseIn(echoPin3, HIGH);
-  distance1 = (duration1 * 0.0343) / 2; // Calculate distance in cm
-  distance2 = (duration2 * 0.0343) / 2; // Calculate distance in cm
-  distance3 = (duration3 * 0.0343) / 2; // Calculate distance in cm
-
-  // Check if any of the sensors detect within 30 cm
-  if (distance1 < 20 && distance2 < 20) {
-    Serial.println("Wall detected on left & right!");
-  }
-
-  if (distance3 < 30) {
-    MotorsSpeed(0,0);
-    if ((b > r))&&(b > g)) {
-      RightSpeed = constrain(RightSpeed, -100, 100);
-      LeftSpeed = constrain(LeftSpeed, 100, -100);
-      yellowBoxCount++;
-      delay(1000)
-      RightSpeed = constrain(RightSpeed, 0, 0);
-      LeftSpeed = constrain(LeftSpeed, 0, 0);
-    }
-  }
-
-  delay(500);
-
-
   Serial.println(Val);
-  
-
-  if (digitalRead(ObjectSens) == LOW) {
-    delay(2);
-    while (digitalRead(ObjectSens) == LOW) {
-      MotorsSpeed(0, 0);
-    }
-  }
 
  // Sensörlerden pozisyon oku
   Position = ReadSensor();
@@ -282,8 +178,8 @@ void loop() {
   LeftSpeed = LeftBaseSpeed - Verify +  ExtraSpeed ;
   
   // Motor hızlarını sınırla
-  RightSpeed = constrain(RightSpeed, -100, 100);
-  LeftSpeed = constrain(LeftSpeed, -100, 100);
+  RightSpeed = constrain(RightSpeed, -175, 175);
+  LeftSpeed = constrain(LeftSpeed, -175,175);
   
   // Motor hızlarını ayarla
   MotorsSpeed(LeftSpeed, RightSpeed);
